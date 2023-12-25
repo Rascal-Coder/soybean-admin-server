b7620c9a1c9802a7fd61b2f18aa38755b7623264
qi yuhang <qiyuhang@thinkerx.com>
2023-12-25 14:36:42 +0800

    chore: 账号登录接口开发

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const svgCaptcha = require('svg-captcha');
-
+/**
+ * 登录验证码
+ */
 router.get('/captcha', function(req, res, next) {
     const width=req.query.width || 100;
     const height=req.query.height || 50;
@@ -15,11 +17,40 @@ router.get('/captcha', function(req, res, next) {
         color: true,
     });
     req.session.captcha = captcha.text;
+    req.session.captchaDate=new Date().getTime();
     res.type('svg');
     res.status(200).send({
         code:200,
         data:captcha.data
     });
 });
-
+/**
+ * 账号登录
+ */
+router.post('/password', function(req, res, next) {
+    const userName=req.query.userName;
+    const password=req.query.password;
+    const captchaText=req.query.captcha;
+    if(!userName || !password ||!captchaText){
+        res.send({
+            code:400,
+            msg:'参数为空',
+            data:null
+        });
+        return;
+    }
+    if(!req.session.captcha 
+        || req.session.captcha !== captchaText 
+        || !req.session.captchaDate 
+        || (Date.now().getTime() - req.session.captchaDate) > 1000*60*5){
+        res.send({
+            code:400,
+            msg:'验证码错误',
+            data:null
+        });
+        return;
+    }
+    // TODO: 登录逻辑,验证用户名和密码,以及jwt生成token返回前端
+    res.send({});
+});
 module.exports = router;
